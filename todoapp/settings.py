2b7dd02ddddea9150488f14c51a11f7cfc446622
Igor <c6h10o5@gmail.com>
2020-02-21 20:19:40 +0300

    Update cache settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -78,29 +78,13 @@ def get_cache():
         username = os.environ['MEMCACHIER_USERNAME']
         password = os.environ['MEMCACHIER_PASSWORD']
         cache = {
-            'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-            'TIMEOUT': 300,
-            'LOCATION': servers,
-            'OPTIONS': {
-                'binary': True,
-                'username': username,
-                'password': password,
-                'behaviors': {
-                    # Enable faster IO
-                    'no_block': True,
-                    'tcp_nodelay': True,
-                    # Keep connection alive
-                    'tcp_keepalive': True,
-                    # Timeout settings
-                    'connect_timeout': 2000,  # ms
-                    'send_timeout': 750 * 1000,  # us
-                    'receive_timeout': 750 * 1000,  # us
-                    '_poll_timeout': 2000,  # ms
-                    # Better failover
-                    'ketama': True,
-                    'remove_failed': 1,
-                    'retry_timeout': 2,
-                    'dead_timeout': 30,
+            'default': {
+                'BACKEND': 'django_bmemcached.memcached.BMemcached',
+                'TIMEOUT': None,
+                'LOCATION': servers,
+                'OPTIONS': {
+                    'username': username,
+                    'password': password,
                 }
             }
         }
