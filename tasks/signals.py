03a0a1dbfc7095458b4f186b1d6edc0f3f43b4f1
uuso <mymail.uuso@gmail.com>
2020-05-03 02:40:33 +0300

    `fff

--- tasks/signals.py
+++ tasks/signals.py
@@ -4,44 +4,20 @@ from tasks.models import TodoItem, Category
 from collections import Counter
 
 
-# @receiver(post_delete, sender=TodoItem)
-# def task_removed(sender, **kwargs):
-#     task_cats_removed('dummy', 'dummy', 'post_remove', 'dummy')
+@receiver(post_delete, sender=TodoItem)
+def task_removed(sender, **kwargs):
+    task_cats_changed('dummy', 'dummy', 'post_remove', 'dummy')
 
 
 @receiver(m2m_changed, sender=TodoItem.category.through)
-def task_cats_added(sender, instance, action, model, **kwargs):
-    print("m2m postadd?")
-    if action != "post_add":
-        return
-    print("entered post_add")
-
-    for cat in instance.category.all():
-        Category.objects.filter(id=cat.id).update(
-            todos_count = TodoItem.objects.filter(category__id=cat.id).count())
-        # slug = cat.slug
-
-        # new_count = 0
-        # for task in TodoItem.objects.all():
-        #     new_count += task.category.filter(slug=slug).count()
-        
-        # Category.objects.filter(slug=slug).update(todos_count=new_count)
-
-
-@receiver(m2m_changed, sender=TodoItem.category.through)
-def task_cats_removed(sender, instance, action, model, **kwargs):
-    print("Got relation: %s" % action)
-    if not action in ("post_remove", "post_clear"):
+def task_cats_changed(sender, instance, action, model, **kwargs):
+    if action == "post_add":
+        qs = instance.category.all()
+    else if action == "post_remove":
+        qs = Category.objects.all()
+    else:
         return
 
-    print("entered post_remove")
-    for cat in Category.objects.all():
+    for cat in qs:
         Category.objects.filter(id=cat.id).update(
             todos_count = TodoItem.objects.filter(category__id=cat.id).count())
-    # cat_counter = Counter()
-    # for t in TodoItem.objects.all():
-    #     for cat in t.category.all():
-    #         cat_counter[cat.slug] += 1
-
-    # for slug, new_count in cat_counter.items():
-    #     Category.objects.filter(slug=slug).update(todos_count=new_count)
