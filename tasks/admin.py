eca00946d89d45ab7a1c22af16d6a126c49fa81b
uuso <mymail.uuso@gmail.com>
2020-05-02 20:47:50 +0300

    aa

--- tasks/admin.py
+++ tasks/admin.py
@@ -8,6 +8,7 @@ class TodoItemAdmin(admin.ModelAdmin):
     list_display = ('description', 'list_categories', 'is_completed', 'created')
     def list_categories(self, obj):
         return list(obj.category.all())
+    list_categories.short_description = 'Список категорий'
 
 
 @admin.register(Category)
