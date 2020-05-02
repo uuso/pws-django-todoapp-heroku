a4a6ef3e64d4e4ac499b1a6b274d64765b6f7c5e
uuso <mymail.uuso@gmail.com>
2020-05-02 17:30:51 +0300

    bad cache func in settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -78,14 +78,12 @@ def get_cache():
         username = os.environ['MEMCACHIER_USERNAME']
         password = os.environ['MEMCACHIER_PASSWORD']
         cache = {
-            'default': {
-                'BACKEND': 'django_bmemcached.memcached.BMemcached',
-                'TIMEOUT': None,
-                'LOCATION': servers,
-                'OPTIONS': {
-                    'username': username,
-                    'password': password,
-                }
+            'BACKEND': 'django_bmemcached.memcached.BMemcached',
+            'TIMEOUT': None,
+            'LOCATION': servers,
+            'OPTIONS': {
+                'username': username,
+                'password': password,
             }
         }
     return {'default': cache}
