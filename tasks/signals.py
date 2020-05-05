5ac12011519839201b4900e88a22e8b6dd2416a5
uuso <mymail.uuso@gmail.com>
2020-05-05 16:30:26 +0300

    888

--- tasks/signals.py
+++ tasks/signals.py
@@ -8,26 +8,31 @@ from collections import Counter
 def task_removed(sender, **kwargs):
     print("\n\ncatched POST_DELETE @ TodoItem\n\n")
     task_cats_changed('dummy', 'dummy', 'post_remove', 'dummy')
-    task_saved(sender)
-
-
-@receiver(post_save, sender=TodoItem)
-def task_saved(sender, **kwargs):
-    print("\n\ncatched POST_SAVE @ TodoItem\n\n")
-
-    todos_qs = TodoItem.objects.prefetch_related('category')
+    # task_saved(sender)
     
-    for cat in Category.objects.all():
-        cat_qs = todos_qs.filter(category__id=cat.id)
+
+# @receiver(post_save, sender=TodoItem)
+# def task_saved(sender, **kwargs):
+#     '''при добавлении таски данный метод сработает после сохранения
+#     самого TodoItem, но до сохранения его M2M поля Category.
+#     Поэтому он не учтёт в подсчете текущий добавленный TodoItem.'''
+    # перенесён в m2m_changed
+    
+
+    # todos_qs = TodoItem.objects.prefetch_related('category')
+    
+    # for cat in Category.objects.all():
+    #     cat_qs = todos_qs.filter(category__id=cat.id)
+    #     print(cat.slug, list(cat_qs))
         
-        prior_high = cat_qs.filter(priority=TodoItem.PRIORITY_HIGH).count()
-        prior_medium = cat_qs.filter(priority=TodoItem.PRIORITY_MEDIUM).count()
-        prior_low = cat_qs.filter(priority=TodoItem.PRIORITY_LOW).count()
+    #     prior_high = cat_qs.filter(priority=TodoItem.PRIORITY_HIGH).count()
+    #     prior_medium = cat_qs.filter(priority=TodoItem.PRIORITY_MEDIUM).count()
+    #     prior_low = cat_qs.filter(priority=TodoItem.PRIORITY_LOW).count()
         
-        cat.prior_high=prior_high
-        cat.prior_medium=prior_medium
-        cat.prior_low=prior_low
-        cat.save()
+    #     cat.prior_high=prior_high
+    #     cat.prior_medium=prior_medium
+    #     cat.prior_low=prior_low
+    #     cat.save()
 
 @receiver(m2m_changed, sender=TodoItem.category.through)
 def task_cats_changed(sender, instance, action, model, **kwargs):
@@ -44,3 +49,19 @@ def task_cats_changed(sender, instance, action, model, **kwargs):
     for cat in qs:
         Category.objects.filter(id=cat.id).update(
             todos_count = TodoItem.objects.filter(category__id=cat.id).count())
+
+    # подсчет тасок по приоритетам и категориям
+    todos_qs = TodoItem.objects.prefetch_related('category')
+    
+    for cat in Category.objects.all():
+        cat_qs = todos_qs.filter(category__id=cat.id)
+        # print(cat.slug, list(cat_qs))
+        
+        prior_high = cat_qs.filter(priority=TodoItem.PRIORITY_HIGH).count()
+        prior_medium = cat_qs.filter(priority=TodoItem.PRIORITY_MEDIUM).count()
+        prior_low = cat_qs.filter(priority=TodoItem.PRIORITY_LOW).count()
+        
+        cat.prior_high=prior_high
+        cat.prior_medium=prior_medium
+        cat.prior_low=prior_low
+        cat.save()
